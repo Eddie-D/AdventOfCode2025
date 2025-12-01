d81d52f9b4d532b95802d89eee15e8f2300f8656
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-01 08:41:21 +0000

    Complete day1 part 1

---
+++ Day01/day1-part1.py
@@ -0,0 +1,11 @@
+file = open("input.txt")
+
+current = 50
+zeroes = 0
+for line in file :
+    dir = -1 if line[0] == "L" else 1
+    amount = int(line[1:])
+    current = (current + (dir * amount)) % 100
+    if current == 0 : zeroes += 1
+
+print(f"There are {zeroes} zeroes")
