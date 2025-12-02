92add0d417437394c09c57dbb7a499728e2e5f33
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-02 10:33:56 +0000

    Clean up day1

--- Day01/day1-part1.py
+++ Day01/day1-part1.py
@@ -5,7 +5,7 @@ zeroes = 0
 for line in file :
     dir = -1 if line[0] == "L" else 1
     amount = int(line[1:])
-    current = (current + (dir * amount)) % 100
+    current = (dir * amount + current) % 100
     if current == 0 : zeroes += 1
 
 print(f"There are {zeroes} zeroes")
--- Day01/day1-part2.py
+++ Day01/day1-part2.py
@@ -7,7 +7,7 @@ for line in file :
     amount = int(line[1:])
     new = (current + (dir * amount))
     # The floor idea works only when second number is bigger than first (56 -> 0 would return 0, but 56 -> 100 returns 1) dir multiplication compensates for this
-    zeroes += abs(((dir *(new)) // 100) - (dir * current // 100))
+    zeroes += abs((dir * new // 100) - (dir * current // 100))
     current = new
 
 print(f"There are {zeroes} zeroes")
