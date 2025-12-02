9d783ea8bcb110f904f20361c61c4aa30a272c53
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-02 10:45:47 +0000

    Complete day2 part2

---
+++ Day02/day2-part2.py
@@ -0,0 +1,31 @@
+from functools import reduce
+from sympy import primefactors
+
+file = open("./input.txt")
+
+total = 0
+for line in file :
+    pairs = [x.split('-') for x in line.split(',')]
+    for start, end in pairs:
+        print(f"Starting {start}")
+        for i in range(int(start), int(end) + 1) :
+            s = str(i)
+            # Trivial case
+            if len(s) == 1 :
+                continue
+
+            # For a breakdown with identical sections, any breakdown of that by a 
+            # divisor of its section size will also work.
+            # So we only need to check prime factors 
+            factors = primefactors(len(s))
+            if factors[0] != i :
+                for p in factors :
+                    size = len(s) // p
+                    sections = [s[(x*size):((x+1)*size)] for x in range(0,p)]
+
+                    if all(map(lambda x: x==sections[0], sections)) :
+                        total += i
+                        i = int(end) + 1
+                        break
+
+print(f"Total is: {total}")
