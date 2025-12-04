ce13c1497fb2159d1f9b668a17e3105be4cae237
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-04 11:04:51 +0000

    Complete day4 part 1

---
+++ Day04/day4-part1.py
@@ -0,0 +1,25 @@
+file = open("input.txt")
+
+lines = file.readlines()
+matrix = [[1 if x == "@" else 0 for x in l.rstrip()]for l in lines]
+numLines = len(lines)
+numCols = len(matrix[0])
+print(f"nl: {numLines}")
+print(f"nc: {numCols}")
+print(f"matrix: {matrix}")
+
+def checkAdjacencies(i,j) :
+    total = 0
+    for a in range(i - 1, i+2) :
+        for b in range(j-1, j+2) :
+            if (a >= 0 and b >= 0 and a < numLines and b < numCols and not (a == i and b == j)) :
+                total += matrix[a][b]
+    return 1 if total < 4 else 0
+
+total = 0
+for i in range(numLines) :
+    for j in range (numCols) :
+        if (matrix[i][j] == 1) :
+            total += checkAdjacencies(i,j)
+
+print(f"Total adjacencies {total}")
