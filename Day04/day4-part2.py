b446dadf0b8715aba9b32ffd3baa0777b87c074c
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-06 09:01:00 +0000

    Complete day4 part2

--- Day04/day4-part2.py
+++ Day04/day4-part2.py
@@ -4,22 +4,31 @@ lines = file.readlines()
 matrix = [[x for x in l.rstrip()]for l in lines]
 numLines = len(lines)
 numCols = len(matrix[0])
-print(f"nl: {numLines}")
-print(f"nc: {numCols}")
-print(f"matrix: {matrix}")
 
-def checkAdjacencies(i,j) :
-    total = 0
-    for a in range(i - 1, i+2) :
-        for b in range(j-1, j+2) :
-            if (a >= 0 and b >= 0 and a < numLines and b < numCols and not (a == i and b == j)) :
-                total += 1 if matrix[a][b] != "." else 0
-    return 1 if total < 4 else 0
+overallTotal = 0
+cycleTotal = 1
+while cycleTotal != 0 :
+    def checkAdjacencies(i,j) :
+        adj = 0
+        for a in range(i - 1, i+2) :
+            for b in range(j-1, j+2) :
+                if (a >= 0 and b >= 0 and a < numLines and b < numCols and not (a == i and b == j)) :
+                    adj += 1 if matrix[a][b] != "." else 0
+        if adj < 4 :
+            matrix[i][j] = "X"
+            return 1
+        return 0
 
-total = 0
-for i in range(numLines) :
-    for j in range (numCols) :
-        if (matrix[i][j] != ".") :
-            total += checkAdjacencies(i,j)
+    cycleTotal = 0
+    for i in range(numLines) :
+        for j in range (numCols) :
+            if matrix[i][j] != "." :
+                cycleTotal += checkAdjacencies(i,j)
 
-print(f"Total adjacencies {total}")
+    for i in range(numLines) :
+        for j in range (numCols) :
+            if matrix[i][j] == "X" : matrix[i][j] = "."
+
+    overallTotal += cycleTotal
+
+print(f"Total adjacencies {overallTotal}")
